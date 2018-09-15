6ac6e0de2d0b2abe38633256e5f951f8f96c14a5
mdivani <georgemdivani.gm@gmail.com>
2018-09-15 15:01:25 +0400

    create base components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './style/base/_base.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
